ec7632e1e18ab9b32883dfb41012a7a1b9cd3e10
Victor Manuel Echavarria <vicme93@gmail.com>
2019-09-22 13:51:44 -0500

    Ejercicio 2 refuerzo de la evaluacion

---
+++ sueldo final/Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace sueldo_final
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtprecio = new System.Windows.Forms.TextBox();
+            this.btnenviar = new System.Windows.Forms.Button();
+            this.lblresultado = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(185, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(431, 32);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Precio de Acuerdo a la Temporada";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioButton1.Location = new System.Drawing.Point(121, 116);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(99, 28);
+            this.radioButton1.TabIndex = 1;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Escolar";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioButton2.Location = new System.Drawing.Point(121, 161);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(112, 28);
+            this.radioButton2.TabIndex = 2;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Hallowen";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioButton3.Location = new System.Drawing.Point(121, 211);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(122, 28);
+            this.radioButton3.TabIndex = 3;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "Diciembre";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(117, 292);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(290, 24);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Ingrese el valor de la Compra:";
+            // 
+            // txtprecio
+            // 
+            this.txtprecio.Location = new System.Drawing.Point(430, 292);
+            this.txtprecio.Multiline = true;
+            this.txtprecio.Name = "txtprecio";
+            this.txtprecio.Size = new System.Drawing.Size(115, 23);
+            this.txtprecio.TabIndex = 5;
+            // 
+            // btnenviar
+            // 
+            this.btnenviar.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnenviar.Location = new System.Drawing.Point(456, 161);
+            this.btnenviar.Name = "btnenviar";
+            this.btnenviar.Size = new System.Drawing.Size(124, 28);
+            this.btnenviar.TabIndex = 6;
+            this.btnenviar.Text = "Calcular";
+            this.btnenviar.UseVisualStyleBackColor = true;
+            this.btnenviar.Click += new System.EventHandler(this.btnenviar_Click);
+            // 
+            // lblresultado
+            // 
+            this.lblresultado.AutoSize = true;
+            this.lblresultado.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblresultado.Location = new System.Drawing.Point(121, 342);
+            this.lblresultado.Name = "lblresultado";
+            this.lblresultado.Size = new System.Drawing.Size(104, 24);
+            this.lblresultado.TabIndex = 7;
+            this.lblresultado.Text = "Resultado";
+            this.lblresultado.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblresultado);
+            this.Controls.Add(this.btnenviar);
+            this.Controls.Add(this.txtprecio);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtprecio;
+        private System.Windows.Forms.Button btnenviar;
+        private System.Windows.Forms.Label lblresultado;
+    }
+}
+
---
+++ sueldo final/Form1.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sueldo_final
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnenviar_Click(object sender, EventArgs e)
+        {
+            double total = double.Parse(txtprecio.Text);
+            string temporada = "";
+            double resultado = double.Parse(txtprecio.Text);
+            if (radioButton1.Checked)
+            {
+                if (total < 100000)
+                {
+
+                    lblresultado.Text = "El Precio Final es:" + resultado.ToString();
+                    lblresultado.Visible = true;
+                   
+                }
+
+                else
+
+                    if  (total <= 300000)
+                {
+                    total = (total * 0.10 );
+                    resultado = (resultado - total);
+                    lblresultado.Text = "El Precio Final es: " + resultado.ToString();
+                    lblresultado.Visible = true;
+                }
+
+
+                else
+                {
+
+                    if (total > 300000)
+                    {
+                        total = (total * 0.15);
+                        resultado = (resultado - total);
+
+                        lblresultado.Text = "El Precio Final es:" + resultado.ToString();
+                        lblresultado.Visible = true;
+                    }
+
+                }
+
+                temporada = radioButton1.Text;
+            }
+
+
+
+            if (radioButton2.Checked)
+            {
+                if (total < 50) {
+
+                    MessageBox.Show("Este Valor es invalido");
+                }
+
+                else 
+                    if ((total >= 50) && (total <= 30000))
+                    {
+
+                        lblresultado.Text = "El Precio Final es:" + total.ToString();
+                        lblresultado.Visible = true;
+
+
+
+                    }
+                
+                else
+                {
+
+                    if ((total>=30001) && (total<= 200000))
+                    {
+                        total = (total * 0.02);
+                        resultado = (resultado - total);
+
+                        lblresultado.Text = "El Precio Final es:" + resultado.ToString();
+                        lblresultado.Visible = true;
+                    }
+                    else
+                    {
+                        if(total> 200000)
+                        {
+                            total = (total * 0.05);
+                            resultado = (resultado - total);
+                            lblresultado.Text = "El Precio Final es:" + resultado.ToString();
+                            lblresultado.Visible = true;
+                        }
+                    }
+                }
+                temporada = radioButton2.Text;
+            }
+            if (radioButton3.Checked)
+            {
+                if((total>=50) && (total<= 300000))
+                {
+                    lblresultado.Text = "El Precio Final es:" + resultado.ToString();
+                    lblresultado.Visible = true;
+                }
+                if (total < 50)
+                {
+
+                    MessageBox.Show("Este Valor es invalido");
+                }
+                else
+                {
+                    if((total>= 300001) && (total<= 500000))
+                    {
+                        total = (total * 0.05);
+                        resultado = (resultado - total);
+
+                        lblresultado.Text = "El Precio Final es:" + resultado.ToString();
+                        lblresultado.Visible = true;
+                    }
+                    else
+                    {
+                        if(total> 500000)
+                        {
+                            total = (total * 0.10);
+                            resultado = (resultado - total);
+
+                            lblresultado.Text = "El Precio Final es:" + resultado.ToString();
+                            lblresultado.Visible = true;
+                        }
+                    }
+                }
+                temporada = radioButton3.Text;
+            }
+            MessageBox.Show(temporada);
+        }
+    }
+
+}
